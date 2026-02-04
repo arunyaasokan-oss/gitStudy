26bbcbf9ec5f6e59272f1a6a5bdd9ab28214d1cd
arunya.asokan <arunya.asokan@trenser.com>
2026-02-04 10:29:57 +0000

    first commit to branch

---
+++ bitoperation.c
@@ -0,0 +1,41 @@
+//*****************************************************************************
+// Copyright(c) 2026 Trenser Technology solution pvt Ltd
+// Allrights are reserved
+//*****************************************************************************
+//
+// File     :   bitoperation.c
+// Summary  :   
+// Note     :   
+// Author   :   Arunya
+// Date     :   04-02-2026
+//************************ Include Files **************************************
+#include <stdio.h>
+#include <stdint.h>
+
+//*************************Local types*****************************************
+
+//*************************Local contants*************************************
+
+//*************************Local variables ************************************
+
+//************************* Local functions ***********************************
+
+//*****************************************************************************
+//Purpose   :Perform bitwise operation
+//Input     :   
+//Output    :
+//Return    :
+//Note      :
+//*****************************************************************************
+void BitOperation(void)
+{
+    uint32_t ulNum1 = 0;
+    uint32_t ulNum2 = 0;
+    uint32_t ulResult = 0;
+
+    ulNum1 = 5;
+    ulNum2 = 4;
+
+    ulResult = ulNum1 & ulNum2;
+    printf("a & b :%u",ulResult);
+}
